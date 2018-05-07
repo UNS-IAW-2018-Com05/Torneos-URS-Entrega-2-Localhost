e426045780caa1c63677616cb8f1194263b99e73
tfelder <tomas.felder@gmail.com>
2018-05-07 17:12:28 -0300

    Solo pueden editar editores

--- app_server/auth/middleware.js
+++ app_server/auth/middleware.js
@@ -1,10 +1,8 @@
 const isAuthenticated = function (req, res, next) {
 
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
+    if (req.isAuthenticated() && req.user.editor)
         return next();
 
-    // if they aren't redirect them to the home page
     res.redirect('/');
 }
 module.exports = isAuthenticated;
--- app_server/routes/editor.js
+++ app_server/routes/editor.js
@@ -4,17 +4,6 @@ var router = express.Router();
 const ctrlEditor = require('../controllers/editor');
 const middleware = require('../auth/middleware');
 
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
-
 /* GET home page. */
-router.get('/',isLoggedIn, ctrlEditor.edicion);
+router.get('/',middleware, ctrlEditor.edicion);
 module.exports = router;
